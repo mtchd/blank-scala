acd81484fe904d053994916743d8ba85f410d3e5
mitchell.paterson <mitchell.paterson@rea-group.com>
2021-09-30 19:28:52 +1000

    rewrite everything

--- src/main/scala/codingtest/Bank.scala
+++ src/main/scala/codingtest/Bank.scala
@@ -1,9 +1,38 @@
 package codingtest
 
-case class Bank(customers: Vector[Customer])
+class Bank(customers: Map[String, BigDecimal]) {
 
-object Bank {
-  def total(bank: Bank): BigDecimal = {
-    bank.customers.foldLeft(BigDecimal(0))((accumulator, customer) => accumulator + customer.bank)
+  def deposit(depositAmt: BigDecimal, customerId: String): Either[AppError, Bank] = {
+
+    val newCustomerBalance = customers.get(customerId) match {
+      case Some(balance) => Right(balance + depositAmt)
+      case None => Left(AppError("Customer not found."))
+    }
+
+    val newCustomers = newCustomerBalance.map(customers.updated(customerId, _))
+    newCustomers.map(new Bank(_))
+  }
+
+  def withdraw(withdrawAmt: BigDecimal, customerId: String): Either[AppError, Bank] = {
+
+    val newCustomerBalance = customers.get(customerId) match {
+      case Some(balance) => Right(balance + withdrawAmt)
+      case None => Left(AppError("Customer not found."))
+    }
+
+    val validCustomerBalance = newCustomerBalance.flatMap(maybeNegativeBalance => {
+      if (maybeNegativeBalance < 0) Left(AppError("Cannot withdraw more than balance."))
+      else Right(maybeNegativeBalance)
+    })
+
+    val newCustomers = validCustomerBalance.map(customers.updated(customerId, _))
+    newCustomers.map(new Bank(_))
+  }
+
+  def checkBalance(customerId: String): Either[AppError, BigDecimal] = {
+    customers.get(customerId) match {
+      case Some(value) => Right(value)
+      case None => Left(AppError("Customer not found."))
+    }
   }
 }
--- src/main/scala/codingtest/Customer.scala
+++ src/main/scala/codingtest/Customer.scala
@@ -1,31 +1,22 @@
 package codingtest
 
-case class Customer(personal: BigDecimal, bank: BigDecimal)
+case class Customer(bank: BigDecimal)
 
 object Customer {
 
-  def deposit(customer: Customer, money: BigDecimal): Either[AppError, Customer] = {
-
-    val newPersonal = customer.personal - money
-    val newBank = customer.bank + money
-
-    val outcome = if (newBank < 0.0) Left(AppError("Customer doesn't have enough money to deposit")) else Right(newPersonal)
-
-    outcome.map(Customer(_, newBank))
+  def deposit(customer: Customer, money: BigDecimal): Customer = {
+    Customer(customer.bank + money)
   }
 
   def withdraw(customer: Customer, money: BigDecimal): Either[AppError, Customer] = {
 
-    val newPersonal = customer.personal + money
     val newBank = customer.bank - money
 
-    val outcome = if (newBank < 0.0) Left(AppError("Customer doesn't have enough money to withdraw")) else Right(newBank)
-
-    outcome.map(Customer(newPersonal, _))
+    if (newBank < 0.0) Left(AppError("Customer doesn't have enough money to withdraw")) else Right(Customer(newBank))
   }
 
   def checkBalance(customer: Customer): BigDecimal = {
-    customer.personal + customer.bank
+    customer.bank
   }
 
 }
