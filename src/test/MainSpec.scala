9378876f00f2177d0d87b708c3b6be8bc7663935
mitchell.paterson <mitchell.paterson@rea-group.com>
2021-11-03 18:07:05 +1100

    Clear tests

--- src/test/MainSpec.scala
+++ src/test/MainSpec.scala
@@ -1,10 +1,7 @@
 import org.specs2.mutable.Specification
-import codingtest.Bank
-import codingtest.CustomerNotFound
-import codingtest.InsufficientFunds
 
 class MainSpec extends Specification {
-  
+
   "Hello" should {
     "Do nothing" in {
       0 mustEqual 0
